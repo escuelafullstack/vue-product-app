40c00d3a1cdb9101f3da2faa29bbed9427d8be2b
Johan Espino <johanespino9@gmail.com>
2020-11-09 01:26:28 -0500

    [ADD] Componentes y props

---
+++ src/components/ProductItem.vue
@@ -0,0 +1,18 @@
+<template>
+    <li id="product-item">{{ title }}</li>
+</template>
+
+<script>
+    export default {
+        name: 'ProductItem',
+        props: {
+            title: String
+        }
+    }
+</script>
+
+<style>
+#product-item {
+    color: blue;
+}
+</style>
---
+++ src/components/ProductList.vue
@@ -0,0 +1,29 @@
+<template>
+    <div id="product-list">
+        <h1>
+            Product List
+        </h1>
+        <ul>
+            <ProductItem title="Zapatilla Nike" />
+            <ProductItem title="Zapatilla Adidas" />
+            <ProductItem title="Zapatilla Puma" />
+        </ul>
+    </div>
+</template>
+
+<script>
+import ProductItem from './ProductItem.vue'
+
+    export default {
+        name: 'ProductList',
+        components: {
+            ProductItem
+        }
+    }
+</script>
+
+<style>
+    #product-list {
+        color: red;
+    }
+</style>
